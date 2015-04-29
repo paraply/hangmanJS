4629197a2cee8ae143228698f807e5e5f404200b
paraply <git@evinja.se>
2015-04-29 09:39:06 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function(){
+    'use strict';
+
+    var myContent = document.getElementById('content');
+
+    myContent.innerHTML = '<h3>Detta är en mall</h3>';
+
+    console.log('Sandbox is ultramega-ready änna!');
+})();
