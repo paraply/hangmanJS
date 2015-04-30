251989224c4396dc46f28377dd4c4a0c4c3e4b5a
paraply <git@evinja.se>
2015-04-30 02:15:54 +0200

    Maybe done V1

--- js/hangman.js
+++ js/hangman.js
@@ -1,94 +1,115 @@
-/**
- * Showing off how to display/hide parts of a SVG-image.
- */
-window.Hangman = (function() {
-    "use strict";
+var Hangman = (function() {
 
-    var hangman = {
+    var characterdiv = document.getElementById('characters');
+    var hangwordsdiv = document.getElementById('hangwords');
+    var trieddiv = document.getElementById('tried');
 
-        // Get all elements as their id
-        "partAsElement": {
-            "hill":     document.getElementById('hang_hill'),
-            "gallow":   document.getElementById('hang_construction'),
-            "body":     document.getElementById('hang_body'),
-            "rightarm": document.getElementById('hang_rightarm'),
-            "leftarm":  document.getElementById('hang_leftarm'),
-            "rightleg": document.getElementById('hang_rightleg'),
-            "leftleg":  document.getElementById('hang_leftleg'),
-            "rope":     document.getElementById('hang_rope'),
-            "head":     document.getElementById('hang_head')
-        },
+    var chrs_found = 0;
+    var tries = 9;
 
-        // Create an array with all valid parts
-        "validParts": [
-            "hill",
-            "gallow",
-            "body",
-            "rightarm",
-            "leftarm",
-            "rightleg",
-            "leftleg",
-            "rope",
-            "head"
-        ],
+    var wordarray = ["KNÄCKT", "SCHLAGER", "CAESAR", "PETRICHOR", "SALTKRÅKA" ];
+    var current_word = wordarray[Math.floor((Math.random() * wordarray.length))];
+
+    var parts = new Array(9);
+        parts[0] =     document.getElementById('hang_hill');
+        parts[1] =   document.getElementById('hang_construction');
+        parts[2] =     document.getElementById('hang_body');
+        parts[3] = document.getElementById('hang_rightarm');
+        parts[4] =  document.getElementById('hang_leftarm');
+        parts[5] = document.getElementById('hang_rightleg');
+        parts[6] =  document.getElementById('hang_leftleg');
+        parts[7] =     document.getElementById('hang_rope');
+        parts[8] =     document.getElementById('hang_head');
+
+    for (var j = 0; j < current_word.length; j++){
+        var guess_char = document.createElement("SPAN");
+        guess_char.setAttribute('id', j);
+        guess_char.className = "guesschr";
+        guess_char.innerHTML = " ";
+        hangwordsdiv.appendChild(guess_char);
+    }
+
+    for (var i = 0; i < 29; i++){ //Loop from the alphabets US ascii codes
+      var currentChar;
+        if (i < 26){ //A -> Z
+            currentChar = String.fromCharCode(65 + i);  
+        }else if (i == 26){ // Swedish Å Ä Ö are not in line with us ascii codes
+             currentChar = "Å"; 
+        }else if (i == 27){
+             currentChar = "Ä"; 
+        }else{
+             currentChar = "Ö"; 
+        }
+        
+        var btn = document.createElement("BUTTON"); //Create a button for every character
+        btn.setAttribute('id', currentChar);
+        btn.className = "unclicked";
+        btn.innerHTML = currentChar;
+        fixClick(btn);
+     
+        characterdiv.appendChild(btn);
+    }
+
+    function fixClick(btn){
+        btn.onclick = function(){
+            char_click(this.id);
+        };
+    }
 
 
-        /**
-         * Check if part a valid part, writes error message to console if the part is invalid.
-         *
-         * @param string part Name of the part to check.
-         *
-         * @returns boolean true if valid part, else false.
-         */
-        "isValid": function (part) {
-
-            if (this.validParts.indexOf(part) === -1) {
-                console.log("The part is not valid: " + part);
-                return false;
+    function char_click(id){ // called when button has been clicked. Argument: character
+        trieddiv.innerHTML += id;
+        var cur_chr = document.getElementById(id);
+        
+        cur_chr.disabled = 'true';
+        cur_chr.innerHTML = '_';
+        var found_word = false;
+            for (var x = 0; x < current_word.length; x++){
+                if (current_word[x] == id){
+                    found_word = true;
+                    chrs_found++;
+                    console.log(id);
+                    var found_chr = document.getElementById(x);
+                    found_chr.innerHTML = current_word[x]  ;
+                }
             }
-            console.log("The part is valid: " + part);
-            return true;
-
-        }, 
-
-
-        /**
-         * Hide a part.
-         *
-         * @param string part Name of the part to hide.
-         *
-         * @returns void.
-         */
-        "hide": function (part) {
-
-            if (this.isValid(part)) {
-                console.log("Hiding part: " + part);
-                this.partAsElement[part].style.display = "none";
+            if (chrs_found == current_word.length){ //Has won
+                alert("You have won the game!");
+                location.reload();
+            }else{ // Not won yet
+                if (!found_word){
+                    tries--;
+                    console.log("Try " + tries);
+                    show(8-tries);
+                if (tries === 0){ //No tries left
+                    alert("You have lost the game!\nThe word was " + current_word);
+                    location.reload();
+                }
             }
-
-        }, 
-
-
-        /**
-         * Show a part.
-         *
-         * @param string part Name of the part to show.
-         *
-         * @returns void.
-         */
-        "show": function (part) {
-
-            if (this.isValid(part)) {
-                console.log("Showing part: " + part);
-                this.partAsElement[part].style.display = "inline";
             }
 
         }
-    };
 
-    console.log("You can now use the hangman object as a part of the window-object. Try\n\nwindow.Hangman.hide('gallow')\nwindow.Hangman.show('gallow')\n\nHere are all the parts you can work on.");
-    console.log(hangman.validParts);
+    function hide (part){
+        (parts[part]).style.display = "none";
+    }
 
-    // Return the object to make it visible.
-    return hangman;
+    function show (part){
+        (parts[part]).style.display = "inline";
+    }
+
+return{
+    peek:function(){ //Public
+        console.log(current_word);
+    },
+
+    hideAll : function(){ //Public
+        for (var i = 0; i < 9; i++){
+            hide(i);
+        }
+    }
+};
+
 })();
+// Hangman.peek();
+Hangman.hideAll();
--- js/main.js
+++ js/main.js
@@ -1,9 +1,75 @@
 (function(){
     'use strict';
 
-    var myContent = document.getElementById('content');
+    var characterdiv = document.getElementById('characters');
+    var hangwordsdiv = document.getElementById('hangwords');
+    var trieddiv = document.getElementById('tried');
+    var wordarray = ["KNÄCKT", "SCHLAGER", "CAESAR", "PETRICHOR" ]
+    var current_word = wordarray[Math.floor((Math.random() * wordarray.length))];
+    var chrs_found = 0;
+    var tries = 9;
 
-    myContent.innerHTML = '<h3>Detta är en mall</h3>';
+    for (var j = 0; j < current_word.length; j++){
+        var guess_char = document.createElement("SPAN");
+        guess_char.setAttribute('id', j);
+        guess_char.className = "guesschr";
+        guess_char.innerHTML = " ";
+        hangwordsdiv.appendChild(guess_char);
+    }
 
-    console.log('Sandbox is ultramega-ready änna!');
+    for (var i = 0; i < 29; i++){
+      var currentChar;
+        if (i < 26){ //A -> Z
+            currentChar = String.fromCharCode(65 + i);  
+        }else if (i == 26){ // Å Ä Ö
+             currentChar = "Å"; 
+        }else if (i == 27){
+             currentChar = "Ä"; 
+        }else{
+             currentChar = "Ö"; 
+        }
+        
+        var btn = document.createElement("BUTTON");
+        btn.setAttribute('id', currentChar);
+        btn.className = "unclicked";
+        btn.innerHTML = currentChar;
+        btn.onclick = function(){
+            char_click(this.id);
+        };
+        characterdiv.appendChild(btn);
+    }
+
+
+    function char_click(id){
+        trieddiv.innerHTML += id;
+        var cur_chr = document.getElementById(id);
+        
+        cur_chr.disabled = 'true';
+        cur_chr.innerHTML = '_';
+        var found_word = false;
+            for (var x = 0; x < current_word.length; x++){
+                if (current_word[x] == id){
+                    found_word = true;
+                    chrs_found++;
+                    console.log(id);
+                    var found_chr = document.getElementById(x);
+                    found_chr.innerHTML = current_word[x]  ;
+                }
+            }
+            if (chrs_found == current_word.length){ //Has won
+                alert("You have won the game!");
+                location.reload();
+            }else{ // Not won yet
+                if (!found_word){
+                    tries--;
+                if (tries == 0){
+                    alert("You have lost the game!\nThe word was " + current_word);
+                    location.reload();
+                }
+            }
+            }
+
+        }
+
+
 })();
