d9465b1b26fc6c7e5a765cb7b55fc7b3da7e162e
Mikael <git@evinja.se>
2015-06-06 18:48:45 +0200

    V2
    Fixed window.hangman. Added wordlist

--- js/hangman.js
+++ js/hangman.js
@@ -7,10 +7,10 @@ var Hangman = (function() {
     var chrs_found = 0;
     var tries = 9;
 
-    var wordarray = ["KNÄCKT", "SCHLAGER", "CAESAR", "PETRICHOR", "SALTKRÅKA" ];
-    var current_word = wordarray[Math.floor((Math.random() * wordarray.length))];
+    var wordarray = ["KNÄCKT", "SCHLAGER", "CAESAR", "PETRICHOR", "SALTKRÅKA" ]; //Word array which the hangman word will be randomly chosen from
+    var current_word = wordarray[Math.floor((Math.random() * wordarray.length))]; //Get a random word
 
-    var parts = new Array(9);
+    var parts = new Array(9); //Body parts as an array
         parts[0] =     document.getElementById('hang_hill');
         parts[1] =   document.getElementById('hang_construction');
         parts[2] =     document.getElementById('hang_body');
@@ -21,36 +21,39 @@ var Hangman = (function() {
         parts[7] =     document.getElementById('hang_rope');
         parts[8] =     document.getElementById('hang_head');
 
-    for (var j = 0; j < current_word.length; j++){
-        var guess_char = document.createElement("SPAN");
-        guess_char.setAttribute('id', j);
-        guess_char.className = "guesschr";
-        guess_char.innerHTML = " ";
-        hangwordsdiv.appendChild(guess_char);
-    }
 
-    for (var i = 0; i < 29; i++){ //Loop from the alphabets US ascii codes
-      var currentChar;
-        if (i < 26){ //A -> Z
-            currentChar = String.fromCharCode(65 + i);  
-        }else if (i == 26){ // Swedish Å Ä Ö are not in line with us ascii codes
-             currentChar = "Å"; 
-        }else if (i == 27){
-             currentChar = "Ä"; 
-        }else{
-             currentChar = "Ö"; 
+    function initHangman(){ //Initialises the game
+        for (var j = 0; j < current_word.length; j++){
+            var guess_char = document.createElement("SPAN");
+            guess_char.setAttribute('id', j);
+            guess_char.className = "guesschr";
+            guess_char.innerHTML = " ";
+            hangwordsdiv.appendChild(guess_char);
         }
-        
-        var btn = document.createElement("BUTTON"); //Create a button for every character
-        btn.setAttribute('id', currentChar);
-        btn.className = "unclicked";
-        btn.innerHTML = currentChar;
-        fixClick(btn);
-     
-        characterdiv.appendChild(btn);
+
+        for (var i = 0; i < 29; i++){ //Loop from the alphabets US ascii codes
+          var currentChar;
+            if (i < 26){ //A -> Z
+                currentChar = String.fromCharCode(65 + i);  
+            }else if (i == 26){ // Swedish Å Ä Ö are not in line with us ascii codes
+                 currentChar = "Å"; 
+            }else if (i == 27){
+                 currentChar = "Ä"; 
+            }else{
+                 currentChar = "Ö"; 
+            }
+            
+            var btn = document.createElement("BUTTON"); //Create a button for every character
+            btn.setAttribute('id', currentChar);
+            btn.className = "unclicked";
+            btn.innerHTML = currentChar;
+            fixClick(btn);
+         
+            characterdiv.appendChild(btn);
+        }
     }
 
-    function fixClick(btn){
+    function fixClick(btn){ //To enable eventlistener for buttons
         btn.onclick = function(){
             char_click(this.id);
         };
@@ -58,14 +61,14 @@ var Hangman = (function() {
 
 
     function char_click(id){ // called when button has been clicked. Argument: character
-        trieddiv.innerHTML += id;
+        trieddiv.innerHTML += id; //Add character to tried character list
         var cur_chr = document.getElementById(id);
         
-        cur_chr.disabled = 'true';
+        cur_chr.disabled = 'true'; //Disable the button so it cannot be clicked again
         cur_chr.innerHTML = '_';
         var found_word = false;
-            for (var x = 0; x < current_word.length; x++){
-                if (current_word[x] == id){
+            for (var x = 0; x < current_word.length; x++){ //Loop through every character from our chosen word
+                if (current_word[x] == id){ // If current character matches
                     found_word = true;
                     chrs_found++;
                     console.log(id);
@@ -98,18 +101,34 @@ var Hangman = (function() {
         (parts[part]).style.display = "inline";
     }
 
-return{
-    peek:function(){ //Public
-        console.log(current_word);
-    },
+    //Prints all words in the wordarray
+    function wordlist(){
+        wordarray.forEach(function(entry){
+            console.log(entry);
+        });        
+    }
 
-    hideAll : function(){ //Public
+    //Returns the current hangman word
+    function peek(){ 
+        console.log(current_word);
+    }
+
+    //Hide all hangman-parts from the image
+    function hideAll(){
         for (var i = 0; i < 9; i++){
             hide(i);
         }
     }
+
+return{ //Export functions and make them public
+    hideAll : hideAll,
+    wordlist : wordlist,
+    init : initHangman,
+    peek : peek
+
 };
 
 })();
 // Hangman.peek();
 Hangman.hideAll();
+Hangman.init();
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(function(){
+window.Hangman =(function(){
     'use strict';
 
     var characterdiv = document.getElementById('characters');
